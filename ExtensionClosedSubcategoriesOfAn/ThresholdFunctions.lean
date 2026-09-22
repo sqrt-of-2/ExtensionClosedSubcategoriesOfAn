837453e93997115b3c0645211f711daf8bd8596c
√2 <240895760+sqrt-of-2@users.noreply.github.com>
2026-09-22 10:03:40 +0000

    fix: restore green build

--- ExtensionClosedSubcategoriesOfAn/ThresholdFunctions.lean
+++ ExtensionClosedSubcategoriesOfAn/ThresholdFunctions.lean
@@ -20,30 +20,27 @@ h ∈ ThresholdFunctions n ↔ (∀i, h i ≤ n - (i.val + 1)) ∧
 (∀i j, i ≤ j → h i ≤ n - (j.val + 1) → h i ≤ h j) := by
   simp [ThresholdFunctions, IsThreshold]
 
-def Threshold_equiv_Bracketing (n : ℕ) : ThresholdFunctions n ≃ bracketingFunctions n :=
+def Threshold_equiv_Bracketing (n : ℕ) : ThresholdFunctions n ≃ BracketingFunctions n :=
 { toFun := fun h =>
-    ⟨fun i => ⟨n - (h.1 i + 1), by omega⟩, by
-      rcases h with ⟨h, hh⟩
-      simp only [mem_ThresholdFunctions] at hh
-      grind [Function.IsBracketing, bracketingFunctions]⟩,
+    ⟨fun i => ⟨n - (h.1 i + 1), by lia⟩, by grind [Function.IsBracketing, mem_ThresholdFunctions]⟩,
   invFun := fun a =>
-    ⟨fun i => ⟨n - (a.1 i + 1), by omega⟩, by
-      grind [mem_bracketingFunctions, IsBracketing, mem_ThresholdFunctions, Fin.mk_eq_zero]⟩,
+    ⟨fun i => ⟨n - (a.1 i + 1), by lia⟩, by grind [Function.IsBracketing, mem_ThresholdFunctions]⟩,
   left_inv := by
-    intro h
-    ext i
+    intro
+    ext
     grind,
   right_inv := by
-    intro a
-    ext i
+    intro
+    ext
     grind }
 
 theorem ThresholdFunctions_card (n : ℕ) : (ThresholdFunctions n).card = catalan n :=
-  (Finset.card_eq_of_equiv (Threshold_equiv_Bracketing n)).trans (bracketingFunctions_card n)
+  (Fintype.card_coe _).symm.trans ((Fintype.card_congr (Threshold_equiv_Bracketing n)).trans
+  (card_bracketingFunctions n))
 
 def Threshold_equiv_Extension_with_all_simples (n : ℕ) :
 ThresholdFunctions n ≃ ext_closed_sets_with_specified_number_projectives n (n + 1) :=
-{ toFun := fun h ↦ ⟨_,_⟩
-  invFun := fun R ↦ ⟨fun i ↦ _,_⟩
+{ toFun := by sorry
+  invFun := by sorry
   left_inv := by sorry
   right_inv := by sorry}
